ed55d5c7d0679995a8f1a2c7fd579c1c44b65691
codaengii2 <carla7039@naver.com>
2023-11-09 23:59:38 +0900

    practice router setting

---
+++ src/Router.js
@@ -0,0 +1,24 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { routes } from "./routes";
+import { PageNotFound } from "./pages/PageNotFound";
+import { Detail } from "./pages/detail/Detail";
+import { Search } from "./pages/search/Search";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+export const Router = () => {
+  return (
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.detail} element={<Detail />} />
+        <Route path={routes.search} element={<Search />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </HashRouter>
+  );
+};
+
+export default Router;
